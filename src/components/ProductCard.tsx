433c8446787de025fb58584e026d31292add7268
Lady Jane <sweet.jane.s@outlook.com>
2026-04-08 12:30:26 -0300

    Basic layout for book cards, need to fix the imports of it all

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,14 +7,19 @@ const ProductCard = () => {
             <div className="category-section">
                 <div className="category-header">
                     <h2>{category.CatName}</h2>
-                    <Link to=
+                    {/*<Link to="/category/CatID"/>*/}
                 </div>
-            </div>
-            <div className="product-book">
-                <h2>{product.prodName}</h2>
+                <div className="product-book">
+                    <h3>I'm {category.product.prodName}!</h3>
+                    <p>Read my story!</p>
+                    <div className="book-img-cover">
+                        {/*<img src={product.prodImgURL} alt={product.prodName}/>*/}
+                    </div>
+                </div>
             </div>
         </>
 
+    //     based on book card by eslam-hany
     );
 };
 
