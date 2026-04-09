8889676ca485c24053eddf9043ac4d7ada809eae
Lady Jane <sweet.jane.s@outlook.com>
2026-04-09 18:25:19 -0300

    Fixed the dashboard, had accidentally removed a necessary item

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -2,38 +2,43 @@ import {loadStripe} from "@stripe/stripe-js";
 import {useCallback} from "react";
 import {EmbeddedCheckoutProvider, EmbeddedCheckout} from '@stripe/react-stripe-js';
 
+import Cookies from "js-cookie";
 
 
 const stripePromise = loadStripe("pk_test_51T2GDsDlTUkLxqzE3nc1QwVYwHPHGUSIUGrKdx8wqz4EaLI6CJ7zg9JK6ickBjwXzHsqrCHl4K4Tqe7p1juursap00tMaihtn5")
 
 const CheckoutPage = () => {
+    const COOKIE_KEY = "shopping_cart";
 
     const fetchClientSecret = useCallback(async ()=> {
+        // get shopping cart from cookie
+        const cart = Cookies.get(COOKIE_KEY)
+
         //create checkout session
         const res = await fetch("http://localhost:8080/checkout/create-checkout-session",{
             method:"POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: cart
         });
         const data = await res.json();
         return data.clientSecret;
     }, []);
+    const options = {fetchClientSecret};
 
     return (
         <>
             <h1>Checkout</h1>
-            <div id="checkout">
-                <EmbeddedCheckoutProvider
-                    stripe={stripePromise}
-                    options={{
-                        fetchClientSecret,
-                        onComplete: () => {
-                            window.location.href = "/confirmation";
-                        }
-                    }}>
-                    <EmbeddedCheckout/>
-                </EmbeddedCheckoutProvider>
-            </div>
+
+            <EmbeddedCheckoutProvider
+                stripe={stripePromise}
+                options={options}
+            >
+                <EmbeddedCheckout />
+            </EmbeddedCheckoutProvider>
         </>
-    );
+    )
 };
 
 export default CheckoutPage;
